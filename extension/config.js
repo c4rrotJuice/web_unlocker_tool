993e014aa11dbc7a7209b38fa68b6b457d8543bb
c4rrotJuice <enoch.freelance@gmail.com>
2026-02-02 22:10:40 +0300

    adding required variables

--- extension/config.js
+++ extension/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_BASE_URL = "https://YOUR_BACKEND_HOST";
-export const SUPABASE_URL = "https://YOUR_SUPABASE_PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+export const BACKEND_BASE_URL = "https://web-unlocker-tool.onrender.com";
+export const SUPABASE_URL = "https://lrdpstewxrufbbgxfcgk.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyZHBzdGV3eHJ1ZmJiZ3hmY2drIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MzcxMzEsImV4cCI6MjA2MzUxMzEzMX0.-qEgFP2HhoO--iuCjslRSBQBHjQY_sGa5Lfx1UU4MBo";

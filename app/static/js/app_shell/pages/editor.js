c8780fc0720d0c876cc876cfe246c6aada3337c1
c4rrotJuice <enoch.freelance@gmail.com>
2026-03-28 18:43:25 +0300

    added versioned imports to force editor refresh

--- app/static/js/app_shell/pages/editor.js
+++ app/static/js/app_shell/pages/editor.js
@@ -1,5 +1,5 @@
 export async function initEditor(boot) {
-  const { createEditorApp } = await import("../../editor_v2/core/editor_app.js");
+  const { createEditorApp } = await import("../../editor_v2/core/editor_app.js?v=20260328b");
   const app = await createEditorApp({ boot });
   await app.start();
 }
